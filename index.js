9526aa42016426f42d7dcafa0b2131a142dc8571
yvan dylan rafanan <rafanandylan@gmail.com>
2024-03-14 14:24:28 +0800

    added a random number generator

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function generateNumber(max) {
         console.log("The number is in B.");
     } else if (randomNumber >= 16 && randomNumber <= 30) {
         console.log("Then number is in I.")
+    } else if (randomNumber >= 31 && randomNumber <= 45) {
+        console.log("The number is in N.");
+    } else if (randomNumber >= 46 && randomNumber <= 60) {
+        console.log("The number is in G.")
+    } else if (randomNumber >= 61 && randomNumber <= 75) {
+        comsole.log("The number is in O.")
     } else {
         console.log("The number is invalid!")
     }
