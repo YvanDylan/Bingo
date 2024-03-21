0cb33a211dd7fca577c1c6225c6c59d77f7dba2b
yvan dylan <rafanandylan@gmail.com>
2024-03-21 14:55:27 +0800

    third

--- index.js
+++ index.js
@@ -1,72 +1,77 @@
 console.log("Hello Pogi!")
 
 let B = false, 
-I = false, 
-N = false, 
-G = false, 
-O = false;
+    I = false, 
+    N = false, 
+    G = false, 
+    O = false;
 
 let BNumber, INumber, NNumber, GNumber, ONumber ;
+let BNumber1, INumber1, NNumber1, GNumber1, ONumber1 ;
+let BNumber2, INumber2, NNumber2, GNumber2, ONumber2 ;
+let BNumber3, INumber3, NNumber3, GNumber3, ONumber3 ;
+let BNumber4, INumber4, NNumber4, GNumber4, ONumber4 ;
 
 function newNumber(maxNumber) {
    let x = Math.floor (Math.random()* maxNumber)+ 1;
-   let b = Math.floor (Math.random() * 15) + 1;
-   let i = Math.floor (Math.random() * 16) + 15;
-   let n = Math.floor (Math.random() * 30) + 30;
-   let g = Math.floor (Math.random() * 60) + 60;
-   let o = Math.floor (Math.random() * 61) + 61;
+   let b = Math.floor (Math.random()* maxNumber)+ 1;
+   let i = Math.floor (Math.random()* maxNumber)+ 1;
+   let n = Math.floor (Math.random()* maxNumber)+ 1;
+   let g = Math.floor (Math.random()* maxNumber)+ 1;
+   
+   
     // if ... esle if ... esle statement
    // conditional operator:
    // GTE > =, LTE < =, EQ ==, NEQ ! =, AND &&, OR ||
 
 
     if (x <= 15){
-        console.log('New number belongs to "B"');
+        console.log('New number ${x} belongs to "B"');
         B = true;
-        BNumber = x;  
+        BNumber = x;   
         document.querySelector("#td1").textContent = x;
-        document.querySelector("#td6").textContent = b;
-        document.querySelector("#td11").textContent = b;
-        document.querySelector("#td16").textContent = b;
-        document.querySelector("#td21").textContent = b;
+        document.querySelector("#td6").textContent = x;
+        document.querySelector("#td11").textContent = x;
+        document.querySelector("#td16").textContent = x;
+        document.querySelector("#td21").textContent = x;
     } else if (x >= 16 && x <= 30) {
-        console.log('New number belongs to "I"');
+        console.log('New number ${x} belongs to "I"');
         I = true;
-        INumber = x;
-        document.querySelector("#td2").textContent = x;
-        document.querySelector("#td7").textContent = i;
-        document.querySelector("#td12").textContent = i;
-        document.querySelector("#td17").textContent = i;
-        document.querySelector("#td22").textContent = i;
+        INumber = b;
+        document.querySelector("#td2").textContent = b;
+        document.querySelector("#td7").textContent = b;
+        document.querySelector("#td12").textContent = b;
+        document.querySelector("#td17").textContent = b;
+        document.querySelector("#td22").textContent = b;
     } else if (x >= 31 && x <= 45) {
-        console.log('New number belongs to "N"');
+        console.log('New number ${x} belongs to "N"');
         N = true;
-        NNumber = x;
-        document.querySelector("#td3").textContent = x;
-        document.querySelector("#td8").textContent = n;
-        document.querySelector("#td13").textContent = n;
-        document.querySelector("#td18").textContent = n;
-        document.querySelector("#td23").textContent = n;
+        NNumber = i;
+        document.querySelector("#td3").textContent = i;
+        document.querySelector("#td8").textContent = i;
+        document.querySelector("#td13").textContent = i;
+        document.querySelector("#td18").textContent = i;
+        document.querySelector("#td23").textContent = i;
     
     } else if (x >= 46 && x <= 60) {
-        console.log('New number belongs to "G"');
+        console.log('New number ${x} belongs to "G"');
         G = true;
-        GNumber = x;
-        document.querySelector("#td4").textContent = x;
-        document.querySelector("#td4").textContent = g;
-        document.querySelector("#td4").textContent = g;
-        document.querySelector("#td4").textContent = g;
-        document.querySelector("#td4").textContent = g;
+        GNumber = n;
+        document.querySelector("#td4").textContent = n;
+        document.querySelector("#td9").textContent = n;
+        document.querySelector("#td14").textContent = n;
+        document.querySelector("#td19").textContent = n;
+        document.querySelector("#td24").textContent = n;
        
     } else if (x >= 61 && x <= 75) {
-        console.log('New number belongs to "O"');
+        console.log('New number ${x} belongs to "O"');
         O = true;
-        ONumber = x;
-        document.querySelector("#td5").textContent = x;
-        document.querySelector("#td5").textContent = o;
-        document.querySelector("#td5").textContent = o;
-        document.querySelector("#td5").textContent = o;
-        document.querySelector("#td5").textContent = o;
+        ONumber = g;
+        document.querySelector("#td5").textContent = g;
+        document.querySelector("#td10").textContent = g;
+        document.querySelector("#td15").textContent = g;
+        document.querySelector("#td20").textContent = g;
+        document.querySelector("#td25").textContent = g;
         
     } else {
         console.log('New number ${x} is invalid.');
